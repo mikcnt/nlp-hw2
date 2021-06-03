Fix bert attention mask.

hw2/stud/models.py
@@ -3,7 +3,11 @@ from torch import nn
 from typing import *
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from torchcrf import CRF
-from transformers import BertModel
+from transformers import (
+    BertModel,
+    BertForTokenClassification,
+    BertForSequenceClassification,
+)
 
 
 def lstm_padded(
@@ -53,7 +57,10 @@ class ABSAModel(nn.Module):
 class ABSABert(nn.Module):
     def __init__(self, hparams):
         super(ABSABert, self).__init__()
-        self.bert = BertModel.from_pretrained("bert-base-cased")
+        # self.bert = BertModel.from_pretrained("bert-base-cased")
+        self.bert = BertForTokenClassification.from_pretrained(
+            "bert-base-cased", num_labels=hparams.num_classes
+        )
         bert_output_dim = self.bert.config.hidden_size
         self.dropout = nn.Dropout(hparams.dropout)
         self.classifier = nn.Linear(bert_output_dim, hparams.num_classes)
@@ -62,11 +69,14 @@ class ABSABert(nn.Module):
 
     def forward(self, x, x_lengths):
         attention_mask = torch.ones_like(x)
-        for i in x_lengths:
-            attention_mask[..., i:] = 0
+        for i, l in enumerate(x_lengths):
+            attention_mask[i, l:] = 0
 
-        output = self.bert(x, attention_mask)["last_hidden_state"]
-        output = self.dropout(output)
+        # output = self.bert(x, attention_mask)["last_hidden_state"]
+        # output = self.dropout(output)
+        #
+        # output = self.classifier(output)
+        # return output
 
-        output = self.classifier(output)
+        output = self.bert(x, attention_mask=attention_mask)["logits"]
         return output