Fix issue with checkpoint, change loss computation

hw2/stud/metrics.py
@@ -26,9 +26,15 @@ class F1SentimentExtraction(Metric):
             tokenizer,
             tagging_schema,
         )
-        self.add_state("tp", default=torch.tensor(0.0), dist_reduce_fx="sum")
-        self.add_state("fp", default=torch.tensor(0.0), dist_reduce_fx="sum")
-        self.add_state("fn", default=torch.tensor(0.0), dist_reduce_fx="sum")
+        self.add_state(
+            "tp", default=torch.tensor(0.0, device="cuda"), dist_reduce_fx="sum"
+        )
+        self.add_state(
+            "fp", default=torch.tensor(0.0, device="cuda"), dist_reduce_fx="sum"
+        )
+        self.add_state(
+            "fn", default=torch.tensor(0.0, device="cuda"), dist_reduce_fx="sum"
+        )
 
     # noinspection PyMethodOverriding
     def update(
@@ -107,9 +113,21 @@ class F1SentimentEvaluation(Metric):
             self.sentiment_types = ["positive", "negative", "neutral", "conflict"]
 
         for sent in self.sentiment_types + ["ALL"]:
-            self.add_state(f"tp_{sent}", default=torch.tensor(0), dist_reduce_fx="sum")
-            self.add_state(f"fp_{sent}", default=torch.tensor(0), dist_reduce_fx="sum")
-            self.add_state(f"fn_{sent}", default=torch.tensor(0), dist_reduce_fx="sum")
+            self.add_state(
+                f"tp_{sent}",
+                default=torch.tensor(0, device="cuda"),
+                dist_reduce_fx="sum",
+            )
+            self.add_state(
+                f"fp_{sent}",
+                default=torch.tensor(0, device="cuda"),
+                dist_reduce_fx="sum",
+            )
+            self.add_state(
+                f"fn_{sent}",
+                default=torch.tensor(0, device="cuda"),
+                dist_reduce_fx="sum",
+            )
 
     def compute_sent(self, sentiment, label, pred):
         if self.mode == "Aspect Sentiment":
@@ -248,7 +266,7 @@ class F1SentimentEvaluation(Metric):
             [scores[ent_type]["r"] for ent_type in self.sentiment_types]
         ) / len(self.sentiment_types)
 
-        return scores["ALL"]["Macro_f1"]
+        return torch.tensor(scores["ALL"]["Macro_f1"], device='cuda')
 
     def reset(self):
         for sent in self.sentiment_types + ["ALL"]:

hw2/stud/models.py
@@ -57,10 +57,10 @@ class ABSAModel(nn.Module):
 class ABSABert(nn.Module):
     def __init__(self, hparams):
         super(ABSABert, self).__init__()
-        # self.bert = BertModel.from_pretrained("bert-base-cased")
-        self.bert = BertForTokenClassification.from_pretrained(
-            "bert-base-cased", num_labels=hparams.num_classes
-        )
+        self.bert = BertModel.from_pretrained("bert-base-cased")
+        # self.bert = BertForTokenClassification.from_pretrained(
+        #     "bert-base-cased", num_labels=hparams.num_classes
+        # )
         bert_output_dim = self.bert.config.hidden_size
         self.dropout = nn.Dropout(hparams.dropout)
         self.classifier = nn.Linear(bert_output_dim, hparams.num_classes)
@@ -72,11 +72,11 @@ class ABSABert(nn.Module):
         for i, l in enumerate(x_lengths):
             attention_mask[i, l:] = 0
 
-        # output = self.bert(x, attention_mask)["last_hidden_state"]
-        # output = self.dropout(output)
-        #
-        # output = self.classifier(output)
-        # return output
+        output = self.bert(x, attention_mask)["last_hidden_state"]
+        output = self.dropout(output)
 
-        output = self.bert(x, attention_mask=attention_mask)["logits"]
+        output = self.classifier(output)
         return output
+
+        # output = self.bert(x, attention_mask=attention_mask)["logits"]
+        # return output

hw2/stud/pl_models.py
@@ -87,9 +87,14 @@ class PlABSAModel(pl.LightningModule):
                 self.model.crf.decode(logits), device=self.device
             )
         else:
-            loss = self.loss_function(
-                logits.view(-1, logits.shape[-1]), labels.view(-1)
-            )
+            mask = labels != self.sentiments_vocabulary["<pad>"]
+            active_loss = mask.view(-1)
+            active_logits = logits.view(-1, self.hparams.num_classes)[active_loss]
+            active_labels = labels.view(-1)[active_loss]
+            loss = self.loss_function(active_logits, active_labels)
+            # loss = self.loss_function(
+            #     logits.view(-1, logits.shape[-1]), labels.view(-1)
+            # )
             predictions = torch.argmax(logits, -1)
         if compute_f1:
             f1_extraction = self.f1_extraction(
@@ -112,6 +117,8 @@ class PlABSAModel(pl.LightningModule):
                 "train_loss": train_loss,
             },
             prog_bar=True,
+            on_step=False,
+            on_epoch=True,
         )
 
         # Return loss to update weights